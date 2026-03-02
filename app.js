3f145c24b447ade42be7e4f514579d2b683e3b44
Sanjivani-Jamkar28 <sanjivani4321@gmail.com>
2026-03-02 16:20:19 +0530

    Refactored to arrow function (partial)

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const login = (user) => {
-  switch (user.role) {
-    case "student":
-      return "Student Dashboard";
-    case "admin":
-      return "Admin Dashboard";
-    default:
-      return "Unauthorized";
+  if (user.role === "student") {
+    return "Student Dashboard";
   }
+  return "Unauthorized";
 };
 module.exports = login;
