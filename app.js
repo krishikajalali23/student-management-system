e217a65b8acaac322910001b925c15501e213856
Sanjivani-Jamkar28 <sanjivani4321@gmail.com>
2026-03-02 15:44:38 +0530

    Added admin authentication support

---
+++ app.js
@@ -0,0 +1,10 @@
+function login(user) {
+  if (user.role === "student") {
+    return "Student Dashboard";
+  }
+  if (user.role === "admin") {
+    return "Admin Dashboard";
+  }
+  return "Unauthorized";
+}
+module.exports = login;
